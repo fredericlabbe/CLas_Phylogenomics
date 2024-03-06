f04a8036f939b119cfe4cae54ce8a71257788e32
Frederic Labbe <frederic.labbe.phd@gmail.com>
2024-03-06 18:19:05 +0400

    Implement the cumulative GC skew calculations

--- GCskew.py
+++ GCskew.py
@@ -4,11 +4,12 @@
 Created on Thu Jan 11 17:31:54 2024
 @author: Frederic Labbe
 This script calculates the incremental and the cumulative GC skew for a given FASTA sequence according to this formula: Skew = (G - C) / (G + C)
-usage: python GCskew.py --sequence /path/to/sequence.fasta' --window 100
+usage: python GCskew.py --sequence '/path/to/sequence.fasta' --window 100
 """
 
 import os
 import pandas as pd
+import numpy as np
 from Bio import SeqIO
 from Bio.SeqUtils import GC_skew
 import argparse
@@ -24,9 +25,10 @@ def GCskew(sequence, window):
         phage = SeqIO.read(open(sequence), 'fasta').seq
         if len(phage) > window and 0 < window:
             gcskew = pd.DataFrame(GC_skew(phage, window = window))
+            cumulative_gcskew = pd.DataFrame(np.cumsum(gcskew))
             coord = pd.DataFrame(list(range(0, len(phage), window)))
-            out = pd.concat([coord, gcskew], axis = 1)
-            out.columns =['Coordinates', 'GCskew']
+            out = pd.concat([coord, gcskew, cumulative_gcskew], axis = 1)
+            out.columns =['Coordinates', 'GCskew', 'Cumulative_GCskew']
             out.to_csv(sequence.removesuffix('.fasta') + '_GCskew_Wind' + str(window) + ".csv", sep = ',', index = False)
         else:
             sys.exit('Error: provide a window size shorter than the FASTA sequence')
